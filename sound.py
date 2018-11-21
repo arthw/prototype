fb202526e34e84bf56245d88938fc2ad9a9b21b8
Neo Zhang <zhang.jianyu@outlook.com>
2018-11-21 16:25:24 +0800

    Create sound.py

---
+++ sound.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.5
+
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+
+sample_rate, samples = wavfile.read('wav/male.wav')
+print(sample_rate)
+print(len(samples))
+
+frequencies, times, spectrogram = signal.spectrogram(samples[:10000], sample_rate)
+
+plt.pcolormesh(times, frequencies, spectrogram)
+plt.imshow(spectrogram)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.show()
